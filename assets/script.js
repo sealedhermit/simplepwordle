d5c72f25a9c7fc7c645f4367fef3b2ffcc9d2c86
michael seaman <mseaman26@gmail.com>
2022-10-07 00:27:45 -0700

    changed button elements to divs because they weren't showing up on pages

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     for(var i = 0; i<30;i++){
-        var letterButton = $("<button>")
+        var letterButton = $("<div>")
         letterButton.attr("class", "letter-button")
         $(".letter-container").append(letterButton)
     }
