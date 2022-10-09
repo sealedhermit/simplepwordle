d910a0461c6c7efac3d79c2f45c5109608b0b858
michael seaman <mseaman26@gmail.com>
2022-10-08 21:55:25 -0700

    got some green functionality going

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
 
+var word = [["Q"], ["E"], ["R"], ["T"], ["Y"]]
+var compareWord = word
 
 var querty = ["Q", "W", "E", "R", "T","Y", "U","I","O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "ENTER", "Z", "X", "C", "V", "B", "N", "M", "DEL"]
 var currentGuess = 0
@@ -19,8 +21,8 @@ function makeTopRowOFKeyboard (){
     for(var i = 0; i < 10;i++){
         keyboardButton = document.createElement("button")
         keyboardButton.setAttribute("class", "keyboard-button container")
+        keyboardButton.setAttribute("id", "key-"+querty[i])
         keyboardButton.innerHTML = querty[i]
-        
         document.getElementById("keyboard-row1").appendChild(keyboardButton)
     }
 }
@@ -28,6 +30,7 @@ function makeMiddleRowOfKeyboard(){
     for(var i = 10; i < 19; i++){
         keyboardButton = document.createElement("button")
         keyboardButton.setAttribute("class", "keyboard-button container")
+        keyboardButton.setAttribute("id", "key-"+querty[i])
         keyboardButton.innerHTML = querty[i]
         document.getElementById("keyboard-row2").appendChild(keyboardButton)
     }
@@ -42,15 +45,28 @@ function MakeBottomRowOfKeyboard(){
     for(var i = 20; i <27; i++){
         var keyboardButton = document.createElement("button")
         keyboardButton.setAttribute("class", "keyboard-button container")
+        keyboardButton.setAttribute("id", "key-"+querty[i])
         keyboardButton.innerHTML = querty[i]
         document.getElementById("keyboard-row3").appendChild(keyboardButton)
     }
 
     var deleteButton = document.createElement("button")
     deleteButton.setAttribute("class", "keyboard-button container big-keyboard-button")
+    deleteButton.setAttribute("id", "key-DEL")
     deleteButton.innerHTML = "DEL"
     document.getElementById("keyboard-row3").appendChild(deleteButton)
 }
+//TODO:still have to figure out what to do with compare word and all that
+function checkForTrueMatches(){
+    for (var i = 0; i < compareWord.length;i++){
+        if(currentUserWord[i] == compareWord[i][0]){
+            document.getElementById("letter-box"+eval(currentGuess*5+i)).setAttribute("class", "green-letter-box")
+            document.getElementById("key-"+compareWord[i][0]).style.backgroundColor = "green"
+        }
+    }
+}
+
+    
 
 //================================When buttons are clicked======
 
@@ -61,14 +77,14 @@ document.getElementById("keyboard-container").addEventListener("click", function
         return
     }
     if(buttonClicked.innerHTML == "DEL"){
-        //TODO: write delete function
+        
         if(currentUserWord.length == 0){
             return
         }
         currentUserWord.pop()
         document.getElementById("letter-box"+currentBoxIndex).innerHTML = ""
         currentBoxIndex -= 1
-        
+        console.log(currentUserWord)
         return
     }
     if(buttonClicked.innerHTML == "ENTER"){
@@ -76,6 +92,9 @@ document.getElementById("keyboard-container").addEventListener("click", function
         if(currentUserWord.length < 5){
             return
         }
+        //check guess against word
+        checkForTrueMatches()
+        console.log(currentBoxIndex)
         currentGuess += 1
         currentUserWord = []
         return
@@ -84,11 +103,12 @@ document.getElementById("keyboard-container").addEventListener("click", function
     if(currentUserWord.length >=5){
         return
     }
-    console.log(currentUserWord)
+    
     currentBoxIndex = (currentGuess * 5)+currentUserWord.length
     console.log(document.getElementById("letter-box"+currentBoxIndex).innerHTML)
     document.getElementById("letter-box"+currentBoxIndex).innerHTML = buttonClicked.innerHTML
     currentUserWord.push(buttonClicked.innerHTML)
+    console.log(currentUserWord)
 
 
 
