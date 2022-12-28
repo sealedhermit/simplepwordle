fbd75801128a2bed75afc9347bdcbd91ff6392b1
michael seaman <mseaman26@gmail.com>
2022-12-28 00:59:56 -0800

    #76 ROGUE 12/28/22

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FETCH"
-let currentWordleNumber = 75  //<---------Change wordle number here
+let wordAsString = "ROGUE"
+let currentWordleNumber = 76  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
