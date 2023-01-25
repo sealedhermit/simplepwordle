41e8df2b2c62fe303890d7b6eab8309eb7119ab5
michael seaman <mseaman26@gmail.com>
2023-01-25 22:06:33 +1100

    #96 OVERT 01/25/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LINGO"
-let currentWordleNumber = 95  //<---------Change wordle number here
+let wordAsString = "OVERT"
+let currentWordleNumber = 96  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
