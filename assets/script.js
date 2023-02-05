2dc83d7398587e745f822c3afc3dce6a8e825971
michael seaman <mseaman26@gmail.com>
2023-02-05 00:06:04 -0800

    #103 RELAY 01/05/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "TRUTH"
-let currentWordleNumber = 102  //<---------Change wordle number here
+let wordAsString = "RELAY"
+let currentWordleNumber = 103  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
