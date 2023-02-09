153625006eee97b88ce37c4383a393c6108eae67
michael seaman <mseaman26@gmail.com>
2023-02-08 20:27:32 -0800

    #106 RINSE 02/09/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "WORLD"
-let currentWordleNumber = 105  //<---------Change wordle number here
+let wordAsString = "RINSE"
+let currentWordleNumber = 106  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
