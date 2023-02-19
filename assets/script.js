c816f81609afa163713bddc6b2a94a7d9d92c1cc
michael seaman <mseaman26@gmail.com>
2023-02-18 20:23:45 -0800

    #115 TREAT 02/19/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ANGRY"
-let currentWordleNumber = 114  //<---------Change wordle number here
+let wordAsString = "TREAT"
+let currentWordleNumber = 115  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
