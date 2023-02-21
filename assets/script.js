18d31a563fd11b1c87825ce15292c7fb1c81014f
michael seaman <mseaman26@gmail.com>
2023-02-20 22:58:44 -0800

    #117 SPREE 02/21/22

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "RAISE"
-let currentWordleNumber = 116  //<---------Change wordle number here
+let wordAsString = "SPREE"
+let currentWordleNumber = 117  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
