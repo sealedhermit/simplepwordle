1cdb64559cd2625ef624601ffce1c4b0aa5d5f97
michael seaman <mseaman26@gmail.com>
2023-02-23 21:23:20 -0800

    #120 ENTER 02/24/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CLOSE"
-let currentWordleNumber = 119  //<---------Change wordle number here
+let wordAsString = "ENTER"
+let currentWordleNumber = 120  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
