85ec42bf4f15d7bdee6a01c5dff79eb597031290
michael seaman <mseaman26@gmail.com>
2023-03-07 22:55:15 -0800

    #129 NIEVE 03/08/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SHIRT"
-let currentWordleNumber = 128  //<---------Change wordle number here
+let wordAsString = "NIEVE"
+let currentWordleNumber = 129  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
