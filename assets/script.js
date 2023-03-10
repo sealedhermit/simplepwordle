f56a9b600421b9227b0f2dbea88853abf491a3e1
michael seaman <mseaman26@gmail.com>
2023-03-10 01:06:44 -0800

    #131 LAGER 03/10/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "RAZOR"
-let currentWordleNumber = 130  //<---------Change wordle number here
+let wordAsString = "LAGER"
+let currentWordleNumber = 131  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
