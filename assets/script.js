46b407fcc1e612985135ede171cd3074fe78238a
michael seaman <mseaman26@gmail.com>
2023-03-16 19:41:05 -0700

    #138 MOUNT 03/17/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LAPSE"
-let currentWordleNumber = 137  //<---------Change wordle number here
+let wordAsString = "MOUNT"
+let currentWordleNumber = 138  //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
