bb65a27ba121e8fb75357d3019317e33932fa66c
michael seaman <mseaman26@gmail.com>
2023-03-20 23:56:28 -0700

    #142 WORSE 03/21/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "FORGE"
-let currentWordleNumber = 141  //<---------Change wordle number here
+let wordAsString = "WORSE"
+let currentWordleNumber = 142 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
