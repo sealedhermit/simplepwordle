d756b54f4bc9574cda463f94af5a4074b743bea3
michael seaman <mseaman26@gmail.com>
2023-03-25 01:37:58 -0700

    #146 GRUNT 03/35/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "DIZZY"
-let currentWordleNumber = 145 //<---------Change wordle number here
+let wordAsString = "GRUNT"
+let currentWordleNumber = 146 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
