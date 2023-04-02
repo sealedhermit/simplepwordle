4aabc8f524712508fedc5dc6e2486f55d7e470a0
michael seaman <mseaman26@gmail.com>
2023-04-02 00:13:36 -0700

    #152 STORY 04/02/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CARGO"
-let currentWordleNumber = 151 //<---------Change wordle number here
+let wordAsString = "STORY"
+let currentWordleNumber = 152 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
