4d0c308b3e6bdd14f7e3ce32895e8362ddfa18b2
michael seaman <mseaman26@gmail.com>
2023-04-09 13:19:58 -0700

    #157 MOVIE 04/09/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "THIRD"
-let currentWordleNumber = 156 //<---------Change wordle number here
+let wordAsString = "MOVIE"
+let currentWordleNumber = 157 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
