173bddeb5f7e0fddfcb4e30d5b967daaac749302
michael seaman <mseaman26@gmail.com>
2023-04-19 10:51:37 -0700

    #166 STRUT 04/19/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CHILL"
-let currentWordleNumber = 165 //<---------Change wordle number here
+let wordAsString = "STRUT"
+let currentWordleNumber = 166 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
