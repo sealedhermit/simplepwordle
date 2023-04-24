02063cde28dc1a7300479209e5d6730e96b43f8c
michael seaman <mseaman26@gmail.com>
2023-04-24 00:14:28 -0700

    #170 MINOR 04/24/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "HOOVE"
-let currentWordleNumber = 169 //<---------Change wordle number here
+let wordAsString = "MINOR"
+let currentWordleNumber = 170 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
