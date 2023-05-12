af58aa574ba47c35b5114bf62f5ce1e058a2e375
michael seaman <mseaman26@gmail.com>
2023-05-12 00:25:46 -0700

    #184 GNOME 04/12/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LOGIC"
-let currentWordleNumber = 183 //<---------Change wordle number here
+let wordAsString = "GNOME"
+let currentWordleNumber = 184 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
