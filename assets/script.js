e0ec6fdcb1cec3b435712c9a37f16f702a02a3fa
michael seaman <mseaman26@gmail.com>
2023-05-14 18:32:30 -0700

    #185 NOISE 05/14/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "GNOME"
-let currentWordleNumber = 184 //<---------Change wordle number here
+let wordAsString = "NOISE"
+let currentWordleNumber = 185 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
