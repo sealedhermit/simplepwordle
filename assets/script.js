df6db33593db7c1c542880680f71898dd1a944c4
michael seaman <mseaman26@gmail.com>
2023-05-17 02:33:20 -0700

    #188 TWIRL 05/17/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "PATIO"
-let currentWordleNumber = 187 //<---------Change wordle number here
+let wordAsString = "TWIRL"
+let currentWordleNumber = 188 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
