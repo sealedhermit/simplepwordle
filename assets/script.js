7d1baf8049f6efd25d792647c8d91dd176c922c0
michael seaman <mseaman26@gmail.com>
2023-05-19 18:19:08 -0700

    #189 EVICT 05/19/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "TWIRL"
-let currentWordleNumber = 188 //<---------Change wordle number here
+let wordAsString = "EVICT"
+let currentWordleNumber = 189 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
