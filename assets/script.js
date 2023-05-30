eed69f3324870d2680e0b75fdd617d013dc11b9c
michael seaman <mseaman26@gmail.com>
2023-05-30 13:12:16 -0700

    #200 MUSIC 05/30/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CLIMB"
-let currentWordleNumber = 199 //<---------Change wordle number here
+let wordAsString = "MUSIC"
+let currentWordleNumber = 200 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
