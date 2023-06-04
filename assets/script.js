aea23442b2e429c92894a80e7c224d9dad347b78
michael seaman <mseaman26@gmail.com>
2023-06-04 01:49:27 -0700

    #205 PLUMB 06/04/23

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SKUNK"
-let currentWordleNumber = 204 //<---------Change wordle number here
+let wordAsString = "PLUMB"
+let currentWordleNumber = 205 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
