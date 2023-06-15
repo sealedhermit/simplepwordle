461c0d32fa30b8eaaf33cf0c2cad0714aec9fbc4
michael seaman <mseaman26@gmail.com>
2023-06-15 00:08:59 -0700

    #213 COMET

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "AGAST"
-let currentWordleNumber = 212 //<---------Change wordle number here
+let wordAsString = "COMET"
+let currentWordleNumber = 213 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
