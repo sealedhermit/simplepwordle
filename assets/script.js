81081ced6f3e2c61f423e10f4d94dc5ef0c9a386
michael seaman <mseaman26@gmail.com>
2023-06-18 12:47:23 -0700

    #216 TIPSY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SHORT"
-let currentWordleNumber = 215 //<---------Change wordle number here
+let wordAsString = "TIPSY"
+let currentWordleNumber = 216 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
