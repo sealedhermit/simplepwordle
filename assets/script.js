ba144429a5854573549d5c17b708500c3efb1b9a
michael seaman <mseaman26@gmail.com>
2023-07-01 14:11:00 -0700

    #225 SWEAT

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "CHARM"
-let currentWordleNumber = 224 //<---------Change wordle number here
+let wordAsString = "SWEAT"
+let currentWordleNumber = 225 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
