1e04422867a787e3037c135d07635c88df962941
michael seaman <mseaman26@gmail.com>
2023-07-04 11:16:32 -0700

    #228 STUDY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SWORD"
-let currentWordleNumber = 227 //<---------Change wordle number here
+let wordAsString = "STUDY"
+let currentWordleNumber = 228 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
