c556b1f343272661de781b763ea61ed26a92a054
michael seaman <mseaman26@gmail.com>
2023-07-19 16:19:00 -0700

    #236 WATER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "RADIO"
-let currentWordleNumber = 235 //<---------Change wordle number here
+let wordAsString = "WATER"
+let currentWordleNumber = 236 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
