8f9e7d98a931bb4b36e0b6af7ea5a86719513c22
michael seaman <mseaman26@gmail.com>
2023-07-21 00:36:38 -0700

    #238 REBEL

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "THROW"
-let currentWordleNumber = 237 //<---------Change wordle number here
+let wordAsString = "REBEL"
+let currentWordleNumber = 238 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
