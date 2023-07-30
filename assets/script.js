9d5a7c2d90e4157fce64fd73b72bcfafbc9c2d7f
michael seaman <mseaman26@gmail.com>
2023-07-30 01:04:10 -0700

    #244 ELDER

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "INPUT"
-let currentWordleNumber = 243 //<---------Change wordle number here
+let wordAsString = "ELDER"
+let currentWordleNumber = 244 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
