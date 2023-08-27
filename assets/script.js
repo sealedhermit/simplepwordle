5dde9820b1278958120b00125e0508e26d2e5c68
michael seaman <mseaman26@gmail.com>
2023-08-27 01:15:21 -0700

    #261 FUROR

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "SWOON"
-let currentWordleNumber = 260 //<---------Change wordle number here
+let wordAsString = "FUROR"
+let currentWordleNumber = 261 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
