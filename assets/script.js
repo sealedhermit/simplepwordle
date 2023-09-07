98e952cf10b0556c3eca90c2fe033aadc8c31276
michael seaman <mseaman26@gmail.com>
2023-09-06 18:27:28 -0700

    #270 SUNNY

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "ALARM"
-let currentWordleNumber = 269 //<---------Change wordle number here
+let wordAsString = "SUNNY"
+let currentWordleNumber = 270 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
