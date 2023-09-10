4b0a35b5098c9b70db27dd390e3a856b53016b17
michael seaman <mseaman26@gmail.com>
2023-09-10 01:48:06 -0700

    #274 CLIFF

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 
 let possibleWordGuessesArray = possibleWordGuessesUnparesed.split(" ")
-let wordAsString = "LOFTY"
-let currentWordleNumber = 273 //<---------Change wordle number here
+let wordAsString = "CLIFF"
+let currentWordleNumber = 274 //<---------Change wordle number here
 
 
 if (usedWords.includes(wordAsString)){
